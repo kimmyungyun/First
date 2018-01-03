e042d2c9ff6fb773a32d22645c68473f81447908
KMY <100angru@gmail.com>
2018-01-03 12:08:31 +0900

    1/3 메인 액티비티 디자인바꿈

--- app/src/main/java/com/example/user/first/MainActivity.java
+++ app/src/main/java/com/example/user/first/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b = (Button)findViewById(R.id.button);
+        ImageButton b = (ImageButton)findViewById(R.id.imageButton);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

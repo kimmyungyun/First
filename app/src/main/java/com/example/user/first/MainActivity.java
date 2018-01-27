08860150e8a38dc96dc004145d67bcde77170910
명윤 <100angru@gmail.com>
2018-01-27 23:46:50 +0900

    1/27 일부 코드 정리.

--- app/src/main/java/com/example/user/first/MainActivity.java
+++ app/src/main/java/com/example/user/first/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.user.first;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import java.util.Timer;
 import java.util.TimerTask;
